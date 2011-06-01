4874d0ae61d84fd658d67068063282f210631a62
Anthony Grimes <i@raynes.me>
2011-06-01 12:58:35 -0500

    Added appending support to the Java serialization backend. Also, added a decode method that supports offset and len. This probably doesn't work properly.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject cereal "0.1.1"
   :description "Dead simple serialization in Clojure."
   :dependencies [[clojure "1.2.0"]
-                 [clojure-useful "0.3.8"]]
+                 [clojure-useful "0.3.8"]
+                 [gloss "0.2.0-alpha1"]]
   :dev-dependencies [[clojure-protobuf "0.4.0"]]
   :tasks [protobuf.tasks])
--- src/cereal/java.clj
+++ src/cereal/java.clj
@@ -1,7 +1,43 @@
 (ns cereal.java
-  (:use cereal.format)
+  (:use cereal.format
+        gloss.core)
+  (:require [gloss.io :as io])
   (:import (java.io ByteArrayInputStream ByteArrayOutputStream
-                    ObjectInputStream ObjectOutputStream)))
+                    ObjectInputStream ObjectOutputStream)
+           java.nio.ByteBuffer))
+
+(defn- unserialize [data]
+  (-> data
+      str
+      .getBytes
+      ByteArrayInputStream.
+      ObjectInputStream.
+      .readObject))
+
+(def netstring
+  (finite-frame
+   (prefix
+    (string-integer :ascii :delimiters [":"])
+    inc
+    dec)
+   (string :utf-8 :suffix ",")))
+
+(defn adjoin
+  "Same as adjoin from clojure-useful, but works with screwed up booleans."
+  [left right]
+  (cond (map? left)
+        (merge-with adjoin left right)
+
+        (and (set? left) (map? right))
+        (reduce
+         (fn [set [k v]]
+           ((if (Boolean/valueOf v) conj disj) set k))
+         left right)
+
+        (coll? left)
+        ((if (coll? right) into conj) left right)
+
+        :else right))
 
 (deftype JavaFormat []
   Format
@@ -9,9 +45,18 @@
   (encode [format data]
     (let [byte-stream (ByteArrayOutputStream.)]
       (.writeObject (ObjectOutputStream. byte-stream) data)
-      (.toByteArray byte-stream)))
+      (.array
+       (io/contiguous
+        (io/encode netstring (String. (.toByteArray byte-stream)))))))
 
   (decode [format data]
-    (.readObject (ObjectInputStream. (ByteArrayInputStream. data)))))
+    (apply merge-with adjoin
+           (map unserialize (io/decode-all netstring data))))
+
+  (decode [format data offset len]
+    (apply merge-with adjoin
+           (map unserialize
+                (io/decode-all netstring
+                               (ByteBuffer/wrap data offset len))))))
 
 (defn make [] (JavaFormat.))
