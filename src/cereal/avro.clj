b314114f8082f9c48d47d0959d2e5cc7fb60df44
Anthony Grimes <i@raynes.me>
2011-06-08 02:40:44 -0500

    Preliminary support for appending by encoding with netstrings.

--- src/cereal/avro.clj
+++ src/cereal/avro.clj
@@ -1,19 +1,23 @@
 (ns cereal.avro
   "Avro backend."
-  (:use cereal.format
-        simple-avro.core))
+  (:use [cereal format netstring]
+        [useful :only [adjoin]]
+        simple-avro.core
+        [clojure.walk :only [stringify-keys]]))
 
-(deftype AvroFormat [schema encoder decoder]
+(deftype AvroFormat [schema]
   Format
+  
   (encode [format data]
-    (pack schema data encoder))
+    (encode-netstring (String. (pack schema (stringify-keys data) binary-encoder))))
+  
   (decode [format data]
-    (unpack schema data decoder)))
+    (apply merge-with adjoin
+           (map (comp #(unpack schema (.getBytes (str %)) binary-decoder))
+                (decode-netstring data)))))
 
 (defn make
   "Create an instance of AvroFormat that uses schema. Optionally takes
    a decoder and encoder."
-  [schema & [encoder decoder]]
-  (AvroFormat. schema
-               (or encoder binary-encoder)
-               (or decoder binary-decoder)))
+  [schema]
+  (AvroFormat. schema))
