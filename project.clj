50027796e71b63b8a4d427c30bc716206d7e2d74
Anthony Grimes <i@raynes.me>
2011-06-09 19:47:58 -0500

    New protobuf version.

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[clojure "1.2.0"]
                  [clojure-useful "0.3.8"]
                  [gloss "0.2.0-alpha1"]]
-  :dev-dependencies [[clojure-protobuf "0.4.0"]]
+  :dev-dependencies [[clojure-protobuf "0.4.4"]]
   :tasks [protobuf.tasks])
