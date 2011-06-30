eaf88c8f553cef2ba41aecaa8ade53577d21594e
Justin Balthrop <git@justinbalthrop.com>
2011-06-29 18:18:15 -0700

    make gloss a dev dep

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject cereal "0.1.5-SNAPSHOT"
   :description "Dead simple serialization in Clojure."
   :dependencies [[clojure "1.2.0"]
-                 [useful "0.4.0"]
-                 [gloss "0.2.0-alpha1"]]
-  :dev-dependencies [[clojure-protobuf "0.4.6"]]
+                 [useful "0.4.0"]]
+  :dev-dependencies [[clojure-protobuf "0.4.6"]
+                     [gloss "0.2.0-alpha1"]]
   :tasks [protobuf.tasks])
