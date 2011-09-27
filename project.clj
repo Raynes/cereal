05db04f5abb5b3e2e1ee49261d477b369a3cc1c1
Alan Malloy <alan@malloys.org>
2011-09-26 18:28:29 -0700

    Update for useful

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cereal "0.1.6"
+(defproject cereal "0.1.7"
   :description "Dead simple serialization in Clojure."
   :dependencies [[clojure "1.2.0"]
-                 [useful "0.5.0"]]
-  :dev-dependencies [[protobuf "0.5.0-alpha6"]
+                 [useful "0.7.0-beta4"]]
+  :dev-dependencies [[protobuf "0.5.0-alpha4"]
                      [gloss "0.2.0-alpha1"]
 ;;                   [cake-marginalia "0.6.0"]
                      [clojure-contrib "1.2.0"]]
