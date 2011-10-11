32f1483922d6106a2b42ab57c21d2961ad5b9749
Justin Balthrop <git@justinbalthrop.com>
2011-10-10 18:28:54 -0700

    0.1.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cereal "0.1.8"
+(defproject cereal "0.1.9"
   :description "Dead simple serialization in Clojure."
   :dependencies [[clojure "1.2.0"]
                  [useful "0.7.0"]]
