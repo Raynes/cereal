ee985888e3ca724e6c423a42ae317a13d7fb2ab5
lance bradley <l@ncebradley.org>
2011-10-21 15:53:43 -0700

    v 0.1.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cereal "0.1.9"
+(defproject cereal "0.1.10"
   :description "Dead simple serialization in Clojure."
   :dependencies [[clojure "1.2.0"]
                  [useful "0.7.0"]]
