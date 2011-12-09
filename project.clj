fd298e580a6ae83c4f458e3f313e1bfa03a5b85f
Anthony Grimes <i@raynes.me>
2011-12-09 15:41:47 -0600

    Updated to 1.3.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cereal "0.2.0-alpha2"
   :description "Revisioned, appendable Gloss codecs."
-  :dependencies [[clojure "1.2.0"]
-                 [gloss "0.2.0-rc1"]
+  :dependencies [[clojure "1.3.0"]
+                 [gloss "0.2.1-SNAPSHOT"]
                  [useful "0.7.5-alpha2"]
-                 [io "0.1.0-alpha1"]])
+                 [io "0.1.0-alpha2"]])
