b2a6f471b882dce03f7b62f48bb644bbfd437c39
Justin Balthrop <git@justinbalthrop.com>
2011-12-12 18:56:50 -0800

    0.2.0-beta1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cereal "0.2.0-alpha2"
+(defproject cereal "0.2.0-beta1"
   :description "Revisioned, appendable Gloss codecs."
   :dependencies [[clojure "1.2.0"]
                  [gloss "0.2.0-rc1"]
-                 [useful "0.7.5-alpha2"]
+                 [useful "0.7.5"]
                  [io "0.1.0-alpha1"]])
