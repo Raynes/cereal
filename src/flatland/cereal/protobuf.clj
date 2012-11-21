dbe7dd6780143931e6663cf153e28017d066e3f1
Alan Malloy <alan@malloys.org>
2012-11-21 15:48:08 -0800

    Sketch out some future ideas with @ninjudd and @lancepantz

--- src/flatland/cereal/protobuf.clj
+++ src/flatland/cereal/protobuf.clj
@@ -49,3 +49,73 @@
                        (fn [encoded-value]
                          [(bit-shift-right encoded-value 3)
                           (bit-and encoded-value 0x7)])))
+
+(comment
+ (def labeled-field
+   (header field-label
+           (fn [[field-number wire-type]]
+             (case write-type
+               0 varint)))))
+
+;; this is probably a monoid?
+(defprotocol Combinable
+  (combine [this other])
+  (default [this]) ;; probably can be shoved into the value function
+  (value [this]))
+
+(defn entries [field-formats]
+  (reify Reader
+    (read-bytes [this b]
+      (let [xs (read-bytes frame b)]
+        (reify
+          Combinable
+          (combine [this other]
+            ...)
+          (value [this]
+            xs)
+          (default [this]
+            (use field formats to build aggregated default)))))))
+
+(comment thinking about making it look like this
+         (proto/message {1 {:key :name, :format (proto/string)}
+                         2 {:key :age, :format (proto/varint)}
+                         3 {:key :jobs
+                            :pre-process vector, :combine into
+                            :format (proto/message {1 {:key :company,
+                                                       :format (proto/string)}
+                                                    2 {:key :years-worked,
+                                                       :format (proto/varint)}})
+                            :post-process (proto/map-by :company)}})
+
+         ;; what about stopping early?
+         ;; folded doesn't actually make the field fold, but decorates the values with
+         ;; a protocol (Combinable) that knows how to combine itself on demand.
+         ;; then we can take-while over that, and the proto/message wrapper is only
+         ;; responsible for making the map-entries into a struct at the end
+         (proto/message
+          (proto/take-while (fn ...)
+                            (proto/entries {1 (-> (proto/field {:key :name :format (proto/string)})
+                                                  (proto/folded ...))
+                                            ...})))
+
+         (proto/message {1 (proto/field {:key :name, :format (proto/string)})
+                         2 (proto/field {:key :age, :format (proto/varint)})
+                         3 (let [map-format (proto/message
+                                             {1 (proto/field {:key :company,
+                                                              :format (proto/string)})
+                                              2 (proto/field {:key :years-worked,
+                                                              :format (proto/varint)})})]
+                             (-> (proto/field {:key :jobs :format map-format})
+                                 (proto/folded {:init {}, :fold (proto/map-by :company map-format)
+                                                :unfold seq})))})
+
+         (-> (proto/message {:name {:key 1, :format (proto/string)}
+                             :age {:key 2, :format (proto/varint)}
+                             :jobs {:key 3
+                                    :pre-process vector, :combine into
+                                    :format (proto/message {1 {:key :company,
+                                                               :format (proto/string)}
+                                                            2 {:key :years-worked,
+                                                               :format (proto/varint)}})
+                                    :post-process (proto/map-by :company)}})
+             (wrap-munge-field :jobs {:fold ... :unfold ...})))
