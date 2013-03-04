3dc5dff992af3bca29651412c5c3f883b6839c6d
Anthony Grimes <i@raynes.me>
2013-03-04 10:51:37 -0800

    Update dependencies. Test against 1.4, 1.5, and 1.6 snapshots.

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Revisioned, appendable Gloss codecs."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.flatland/useful "0.9.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.flatland/useful "0.9.4"]
                  [org.flatland/io "0.3.0"]
-                 [gloss "0.2.1"]]
-  :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.5,default" "test"]}
-  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+                 [gloss "0.2.2-beta5"]]
+  :aliases {"testall" ["with-profile" "dev,default:dev,1.4,default:dev,1.6,default" "test"]}
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
   :repositories {"sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}})
